c4a74d02015fe831963d66ad5435039a48d5ff4d
何伟庚 <6318275@qq.com>
2016-12-22 23:42:39 +0800

    Initialization

---
+++ utility/ClassLibraryTests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void unitTestTest()
+        {
+            Class1 c = new Class1();
+            int b = c.unitTest();
+            Assert.IsTrue(b==1010);
+        }
+    }
+}
